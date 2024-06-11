785e3b41f35e3af3e804cb1a055afa1210ede6f3
Kaique de Oliveira <kaiquedeoliveira@Air-de-Kaique.Dlink>
2024-06-11 17:39:21 -0300

    funcao de cadastrar horas trabalhadas adicionada

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:login/screens/home_screen.dart';
 import 'package:login/screens/login_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -18,13 +20,36 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Horas V3',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        primarySwatch: Colors.blue,
         useMaterial3: true,
       ),
-      home: LoginScreen(),
+      home: const RoteadorTelas(),
     );
+  }
+}
+
+class RoteadorTelas extends StatelessWidget {
+  const RoteadorTelas({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+        stream: FirebaseAuth.instance.userChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else {
+            if (snapshot.hasData) {
+              return HomeScreen(user: snapshot.data!);
+            } else {
+              return LoginScreen();
+            }
+          }
+        });
   }
 }
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,224 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:login/helpers/hour_helpers.dart';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
+import 'package:uuid/uuid.dart';
+
+import '../components/menu.dart';
+import '../models/hour.dart';
+
+class HomeScreen extends StatefulWidget {
+  final User user;
+
+  HomeScreen({super.key, required this.user});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<Hour> listHours = [];
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Menu(user: widget.user),
+      appBar: AppBar(
+        title: const Text('Horas V3'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showFormModal();
+        },
+        child: const Icon(Icons.add),
+      ),
+      body: (listHours.isEmpty)
+          ? const Center(
+              child: Text(
+                'Nada por aqui.\nVamos registrar um dia de trabalho?',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+            )
+          : ListView(
+              padding: const EdgeInsets.only(left: 4, right: 4),
+              children: List.generate(
+                listHours.length,
+                (index) {
+                  Hour model = listHours[index];
+                  return Dismissible(
+                    key: ValueKey<Hour>(model),
+                    direction: DismissDirection.endToStart,
+                    background: Container(
+                      alignment: Alignment.centerRight,
+                      padding: const EdgeInsets.only(right: 12),
+                      color: Colors.red,
+                      child: const Icon(
+                        Icons.delete,
+                        color: Colors.white,
+                      ),
+                    ),
+                    onDismissed: (direction) {
+                      remove(model);
+                    },
+                    child: Card(
+                      elevation: 2,
+                      child: Column(
+                        children: [
+                          ListTile(
+                            onLongPress: () {},
+                            onTap: () {},
+                            leading: const Icon(
+                              Icons.list_alt_rounded,
+                              size: 56,
+                            ),
+                            title: Text(
+                                "Data: ${model.data} hora: ${HourHelper.minutesTohours(model.minutos)}"),
+                            subtitle: Text(model.descricao!),
+                          )
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+    );
+  }
+
+  showFormModal({Hour? model}) {
+    String title = "Adicionar";
+    String confirmationButton = "Salvar";
+    String skipButton = "Cancelar";
+
+    TextEditingController dataController = TextEditingController();
+    final dataMaskFormatter = MaskTextInputFormatter(mask: '##/##/####');
+    TextEditingController minutosController = TextEditingController();
+    final minutosMaskFormatter = MaskTextInputFormatter(mask: '##:##');
+    TextEditingController descricaoController = TextEditingController();
+
+    if (model != null) {
+      title = "Editando";
+      dataController.text = model.data;
+      minutosController.text = HourHelper.minutesTohours(model.minutos);
+      if (model.descricao != null) {
+        descricaoController.text = model.descricao!;
+      }
+    }
+
+    showModalBottomSheet(
+      context: context,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(
+          top: Radius.circular(24),
+        ),
+      ),
+      builder: (context) {
+        return Container(
+          height: MediaQuery.of(context).size.height,
+          padding: const EdgeInsets.all(32),
+          child: ListView(
+            children: [
+              Text(title, style: Theme.of(context).textTheme.headlineSmall),
+              TextFormField(
+                controller: dataController,
+                keyboardType: TextInputType.datetime,
+                decoration: const InputDecoration(
+                  hintText: '01/01/2024',
+                  labelText: 'Data',
+                ),
+                inputFormatters: [dataMaskFormatter],
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              TextFormField(
+                controller: minutosController,
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                    hintText: '00:00', labelText: 'Horas trabalhadas'),
+                inputFormatters: [minutosMaskFormatter],
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              TextFormField(
+                controller: descricaoController,
+                keyboardType: TextInputType.text,
+                decoration: const InputDecoration(
+                    hintText: 'Lembrete do que você fez',
+                    labelText: 'Descrição'),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text(
+                      skipButton,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 16,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      Hour hour = Hour(
+                          id: const Uuid().v1(),
+                          data: dataController.text,
+                          minutos: HourHelper.hoursToMinutos(
+                            minutosController.text,
+                          ));
+
+                      if (descricaoController.text != "") {
+                        hour.descricao = descricaoController.text;
+                      }
+
+                      if (model != null) {
+                        hour.id = model.id;
+                      }
+
+                      firestore
+                          .collection(widget.user.uid)
+                          .doc(hour.id)
+                          .set(hour.toMap());
+
+                      refresh();
+
+                      Navigator.pop(context);
+                    },
+                    child: Text(confirmationButton),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 180,
+              )
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  void remove(Hour model) {
+    firestore.collection(widget.user.uid).doc(model.id).delete();
+    refresh();
+  }
+
+  void refresh() {}
+}
